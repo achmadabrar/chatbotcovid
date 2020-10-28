1f634ed77c7f83f746b7cc509185f0480897869b
achmad <achmad.abrar@bridestory.com>
2020-10-28 17:24:06 +0700

    add hide keyboard

--- app/src/main/java/com/tokopedia/durianmoney_covid_chatbot/external/Extensions.kt
+++ app/src/main/java/com/tokopedia/durianmoney_covid_chatbot/external/Extensions.kt
@@ -1,24 +1,16 @@
 package com.tokopedia.durianmoney_covid_chatbot.external
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.LiveData
 import kotlinx.coroutines.Dispatchers
 import com.tokopedia.durianmoney_covid_chatbot.data.networks.Results
 import com.tokopedia.durianmoney_covid_chatbot.data.networks.Results.Companion.loading
 
 
-/*
-fun <T, A> resultLiveData(databaseQuery: () -> LiveData<T>,
-                          networkCall: suspend () -> Results<A>,
-                          saveCallResult: suspend (A) -> Unit): LiveData<Results<T>> =
-    liveData(Dispatchers.IO) {
-        emit(loading<T>())
-        val source = databaseQuery.invoke().map { Results.success(it) }
-        emitSource(source)
-        val responseStatus = networkCall.invoke()
-        if (responseStatus.status == Results.Status.SUCCESS) {
-            saveCallResult(responseStatus.data!!)
-        } else if (responseStatus.status == Results.Status.ERROR) {
-            emit(Results.error<T>(responseStatus.message!!))
-            emitSource(source)
-        }
-    }*/
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
